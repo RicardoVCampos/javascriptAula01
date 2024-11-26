d2d4ec346be581fc8c1082580f04cac3338b99bb
RicardoVCampos <rvc.top@gmail.com>
2024-11-26 19:09:15 -0300

    agora

---
+++ app.js
@@ -0,0 +1,27 @@
+// let palavra1 = 'Java'
+// let palavra2 = ' Script'
+// let palavra3 = ' é muito'
+// let palavra4 = ' massa'
+// console.log(palavra1+palavra2+palavra3+palavra4)
+
+// let palavra1 = 'Oi'
+// let palavra2 = 'Raimundo'
+// let palavra3 = "Fs20."
+// console.log(`${palavra1} pessoal, aqui é o ${palavra2} da ${palavra3} e essa aula ta massa.`)
+
+// let var1 = 'Ganhar'
+// let var2 = 'Perder'
+// console.log(`Nem quem ${var1} ou ${var2} vai ${var1} ou ${var2}, vai todo mundo ${var2}`)
+
+// let variavel1 = 'Texto'
+// let numero1 = 10
+// let booleano = true
+// let indefinido
+// let nulo = null;
+// let lista = ['oi', 'som', true, null, 20]
+// let lista2 = [1, 23, 4,5,6]
+// let lista3 = [1, 'oi som',true,null,20]
+
+// console.log(lista3[1]+ [2])
+
+// let lista = [1, 2, 3, 4, 5, 6]
